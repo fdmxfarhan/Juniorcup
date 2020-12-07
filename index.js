edad9c698fc095d0dac1a3b0ba872d8cdbfa0f4f
fdmxfarhan <fdmxfarhan@yahoo.com>
2020-12-07 13:47:40 +0330

    FirstCommit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+// routs requirement
+var indexRouter = require('./routes/index');
+
+// port setup
+const port = 3000
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// public path setup
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes Handlers
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    console.log(err);
+    if(!req.user) res.render('error',{
+        uname: false,
+        user: false,
+        err
+    });
+    else res.render('error', {
+        uname: req.user.uname,
+        user: req.user,
+        err
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Juniorcup is started at port ${port}`);
+});
