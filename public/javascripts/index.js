19a7be5c9dd58a67fe6a524a74f7886cda9a8dd9
fdmxfarhan <fdmxfarhan@yahoo.com>
2020-12-11 01:37:08 +0330

    m

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-    
+    var Showinfo = false;
+    $('#info-text').hide();
+    $('#info-img').hide();
+    $(window).scroll(function (event) {
+        var scroll = $(window).scrollTop();
+        // alert(scroll);
+        if(scroll > 400 && !Showinfo)
+        {
+            $('#info-text').fadeIn(1000);
+            $('#info-img').fadeIn(1000);
+            Showinfo = true;
+        }
+    });
 });
