3b9fae4bc0674cf27956edb442b836bd6138d238
fdmxfarhan <fdmxfarhan@yahoo.com>
2020-12-11 22:45:33 +0330

    m

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,15 +1,25 @@
 $(document).ready(function(){
-    var Showinfo = false;
-    $('#info-text').hide();
-    $('#info-img').hide();
-    $(window).scroll(function (event) {
-        var scroll = $(window).scrollTop();
-        // alert(scroll);
-        if(scroll > 400 && !Showinfo)
-        {
-            $('#info-text').fadeIn(1000);
-            $('#info-img').fadeIn(1000);
-            Showinfo = true;
-        }
+    $('.navbar-toggler').click(() => {
+        $('#navbarSupportedContent').show(1000);
+        $('.transparent-modal').show();
     });
+    $('.transparent-modal').click(() => {
+        $('#navbarSupportedContent').hide();
+        $('.transparent-modal').hide();
+    });
+
+
+    // var Showinfo = false;
+    // $('#info-text').hide();
+    // $('#info-img').hide();
+    // $(window).scroll(function (event) {
+    //     var scroll = $(window).scrollTop();
+    //     // alert(scroll);
+    //     if(scroll > 400 && !Showinfo)
+    //     {
+    //         $('#info-text').fadeIn(1000);
+    //         $('#info-img').fadeIn(1000);
+    //         Showinfo = true;
+    //     }
+    // });
 });
