757d0eb5d303f6be2b27e2a705e8d0d59903d945
fdmxfarhan <fdmxfarhan@yahoo.com>
2020-12-12 20:50:05 +0330

    m

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(3000);
-httpsServer.listen(8443);
+httpsServer.listen(3000);
 
 // app.listen(port, () => {
 //   console.log(`Juniorcup is started at port ${port}`);
