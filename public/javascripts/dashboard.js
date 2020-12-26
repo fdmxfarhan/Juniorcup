832d95055f26e234e9f5f0e24c44750c6dac0a89
fdmxfarhan <fdmxfarhan@yahoo.com>
2020-12-26 18:39:26 +0330

    m

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,14 +1,35 @@
 $(document).ready(function(){
-    $('.navigation').mouseenter(()=>{
-        $('.dashboard').css('width', '87%');
-    });
-    $('.navigation').mouseleave(()=>{
-        $('.dashboard').css('width', '94%');
-    });
+    if($(window).width() > 800){
+        $('.navigation').mouseenter(()=>{
+            $('.dashboard').css('width', '87%');
+        });
+        $('.navigation').mouseleave(()=>{
+            $('.dashboard').css('width', '94%');
+        });
+    }
+    else{
+        $('.navigation').mouseenter(()=>{
+            $('.dashboard').css('width', '60%');
+        });
+        $('.navigation').mouseleave(()=>{
+            $('.dashboard').css('width', '90%');
+        });
+    }
     $('a.user-login').mouseenter(()=>{
-        $('.user-action').show(1000);
+        $('.user-action').show(800);
+        // $('.transparent-modal').show();
     });
     $('.user-action').mouseleave(()=>{
         $('.user-action').hide(500);
     });
+    $('button.navbar-toggler').click(()=>{
+        $('#navbarSupportedContent.collapse').show(800);
+        $('.transparent-modal').show();
+    });
+    $('.transparent-modal').click(()=>{
+        $('#navbarSupportedContent.collapse').hide(500);
+        $('.transparent-modal').hide();
+        // $('.user-action').hide(500);
+    });
+    
 });
