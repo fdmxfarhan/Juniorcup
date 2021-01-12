0f63f7e3c4bc472a13a1928a7c720bdf1063f7ee
fdmxfarhan <fdmxfarhan@yahoo.com>
2021-01-12 12:12:32 +0330

    m

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
-
+var User = require('../models/User');
 
 router.get('/', ensureAuthenticated,(req, res, next) => {
     res.render('./dashboard/user-dashboard',{
         user: req.user
+    });
+});
+router.get('/users',(req, res, next) => {
+    User.updateMany({username: 'fdmxfarhan'}, {$set: {role: 'admin'}}, (err, res) => {
+        if(err) console.log(err);
+    })
+    User.find({}, (err, users) => {
+        if(err) console.log(err);
+        console.log(users);
+        res.render('./dashboard/users',{
+            users
+        });
     });
 });
 
