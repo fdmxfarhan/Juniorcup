e15c2fc1120cec09f2c0099280ab6e00236836c9
fdmxfarhan <fdmxfarhan@yahoo.com>
2021-04-13 10:37:35 +0430

    m

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 var Gallery = require('../models/Gallery');
 const Team = require('../models/Team');
 const Certificate = require('../models/Certificate');
+var excel = require('excel4node');
 
 const competitionDate = new Date('April 15, 2021 07:00:00');
 
@@ -160,6 +162,7 @@ router.post('/edit-info', (req, res, next) => {
     Team.find({}, (err, teams) => {
         teams.forEach(team => {
             team.members.forEach(member => {
+                // console.log(member);
                 if(member.idNumber == req.body.idNumber)
                 {
                     rendered = true;
@@ -176,26 +179,86 @@ router.post('/edit-info', (req, res, next) => {
 });
 
 router.post('/certificate', (req, res, next) => {
-    var {idNumber, englishFirstName, englishLastName, englishAffiliation, league, phoneNumber, teamName, teamID} = req.body;
-    if(!idNumber || !englishFirstName || !englishLastName || !englishAffiliation || !league || !phoneNumber || !teamName || !teamID)
+    var {idNumber, englishFirstName, englishLastName, englishAffiliation, persianLeague, phoneNumber, teamName, teamID, persianName, persianAffiliation} = req.body;
+    if(!idNumber || !englishFirstName || !englishLastName || !englishAffiliation || !persianLeague || !phoneNumber || !teamName || !teamID || !persianName || !persianAffiliation)
     {
-        res.send('error');
+        res.send('fill the blanks please');
     }
     else
     {
-        var englishLeague;
-        if(league == "فوتبالیست سبک وزن primary")    englishLeague = 'Soccer Lihgt Weight Primary'           
-        if(league == "فوتبالیست سبک وزن secondary")  englishLeague = 'Soccer Lihgt Weight Secondary'             
-        if(league == "فوتبالیست وزن آزاد")           englishLeague = 'Soccer Open Weight'   
-        if(league == "امداد فضای مشترک")             englishLeague = 'Cospace'   
-        if(league == "برنامه نویسی")                 englishLeague = 'Programming'   
-        if(league == "خودروهای هوشمند")              englishLeague = 'Smart Cars'   
-        if(league == "فوتبال ۲ بعدی")                englishLeague = 'Soccer 2D'   
-        if(league == "virtual rescue")               englishLeague = 'Virtual Rescue'  
-        const newCertificate = new Certificate({idNumber, englishFirstName, englishLastName, englishAffiliation, englishLeague, phoneNumber, teamName, teamID});
-        newCertificate.save().then(doc => {
-            res.render('./certificate/seccess');
-        }).catch(err => {if(err) console.log(err);});
+        Certificate.deleteMany({idNumber: idNumber}, (err) => {
+            var englishLeague;
+            if(persianLeague == "فوتبالیست سبک وزن primary")    englishLeague = 'Soccer Lihgt Weight Primary'           
+            if(persianLeague == "فوتبالیست سبک وزن secondary")  englishLeague = 'Soccer Lihgt Weight Secondary'             
+            if(persianLeague == "فوتبالیست وزن آزاد")           englishLeague = 'Soccer Open Weight'   
+            if(persianLeague == "امداد فضای مشترک")             englishLeague = 'Cospace'   
+            if(persianLeague == "برنامه نویسی")                 englishLeague = 'Programming'   
+            if(persianLeague == "خودروهای هوشمند")              englishLeague = 'Smart Cars'   
+            if(persianLeague == "فوتبال ۲ بعدی")                englishLeague = 'Soccer 2D'   
+            if(persianLeague == "virtual rescue")               englishLeague = 'Virtual Rescue'  
+            const newCertificate = new Certificate({
+                idNumber, 
+                englishFirstName, 
+                englishLastName, 
+                englishAffiliation, 
+                englishLeague, 
+                phoneNumber, 
+                teamName, 
+                teamID, 
+                persianName, 
+                persianLeague,
+                persianAffiliation
+            });
+            newCertificate.save().then(doc => {
+                res.render('./certificate/seccess');
+            }).catch(err => {if(err) console.log(err);});
+        });
+    }
+});
+
+router.get('/export-certificate', ensureAuthenticated, (req, res, next) => {
+    if(req.user.role == 'admin')
+    {
+        Certificate.find({}, (err, certificates) => {
+            var workbook = new excel.Workbook();
+            var worksheet = workbook.addWorksheet('Sheet 1');
+            var style = workbook.createStyle({
+                font: {
+                color: '#FF0800',
+                size: 12
+                },
+                numberFormat: '$#,##0.00; ($#,##0.00); -'
+            });
+            worksheet.cell(1,1).string( `idNumber`).style(style);
+            worksheet.cell(1,2).string( `englishFirstName`).style(style);
+            worksheet.cell(1,3).string( `englishLastName`).style(style);
+            worksheet.cell(1,4).string( `englishLeague`).style(style);
+            worksheet.cell(1,5).string( `englishAffiliation`).style(style);
+            worksheet.cell(1,6).string( `teamName`).style(style);
+            worksheet.cell(1,7).string( `persianName`).style(style);
+            worksheet.cell(1,8).string( `persianLeague`).style(style);
+            worksheet.cell(1,9).string( `persianAffiliation`).style(style);
+            worksheet.cell(1,10).string(`phoneNumber`).style(style);
+            worksheet.cell(1,11).string(`teamID`).style(style);
+            for (let i = 0; i < certificates.length; i++) {
+                worksheet.cell(i+2,1).string( `${certificates[i].idNumber}`).style(style);
+                worksheet.cell(i+2,2).string( `${certificates[i].englishFirstName}`).style(style);
+                worksheet.cell(i+2,3).string( `${certificates[i].englishLastName}`).style(style);
+                worksheet.cell(i+2,4).string( `${certificates[i].englishLeague}`).style(style);
+                worksheet.cell(i+2,5).string( `${certificates[i].englishAffiliation}`).style(style);
+                worksheet.cell(i+2,6).string( `${certificates[i].teamName}`).style(style);
+                worksheet.cell(i+2,7).string( `${certificates[i].persianName}`).style(style);
+                worksheet.cell(i+2,8).string( `${certificates[i].persianLeague}`).style(style);
+                worksheet.cell(i+2,9).string( `${certificates[i].persianAffiliation}`).style(style);
+                worksheet.cell(i+2,10).string(`${certificates[i].phoneNumber}`).style(style);
+                worksheet.cell(i+2,11).string(`${certificates[i].teamID}`).style(style);
+            }
+            workbook.write('./public/certificate.xlsx');
+
+            res.render('./certificate/export',{
+                certificates
+            })
+        });
     }
 });
 
