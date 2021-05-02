a2ec809147dedc0d76b2daebd3211f35daa33912
fdmxfarhan <fdmxfarhan@yahoo.com>
2021-05-02 23:43:46 +0430

    m

--- public/javascripts/ari.js
+++ public/javascripts/ari.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     $('#second').delay(5000).fadeIn(1000).delay(2000).fadeOut(1000);
     $('#third').delay(9000).fadeIn(1000).delay(2000).fadeOut(1000);
     $('#fourth').delay(13000).fadeIn(1000);
+    $('.timer').delay(13000).fadeIn(1000);
     setTimeout(function() {
         $('body').css('animation-name', 'fade');
         $('body').css('background-color', 'purple');
