dd66820230db20f4cfdff479ac4da11dea152ae1
Farhan <fdmxfarhan@yahoo.com>
2021-05-02 18:31:55 -0400

    asdf

--- public/javascripts/ari.js
+++ public/javascripts/ari.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-    $('#first').delay(1000).fadeIn(1000).delay(3000).fadeOut(1000);
-    $('#second').delay(6000).fadeIn(1000).delay(3000).fadeOut(1000);
-    $('#third').delay(11000).fadeIn(1000).delay(3000).fadeOut(1000);
-    $('#fourth').delay(16000).fadeIn(1000);
+    $('#first').delay(1000).fadeIn(1000).delay(2000).fadeOut(1000);
+    $('#second').delay(5000).fadeIn(1000).delay(2000).fadeOut(1000);
+    $('#third').delay(9000).fadeIn(1000).delay(2000).fadeOut(1000);
+    $('#fourth').delay(13000).fadeIn(1000);
     setTimeout(function() {
         $('body').css('animation-name', 'fade');
         $('body').css('background-color', 'purple');
-    }, 16000);
+    }, 13000);
 });
