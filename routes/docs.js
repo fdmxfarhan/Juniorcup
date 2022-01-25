2c0c76de40689d4f2fa427b9aefc7d1afa57a80d
Farhan Daemi <fdmxfarhan@yahoo.com>
2022-01-25 17:10:49 +0330

    asdf

--- routes/docs.js
+++ routes/docs.js
@@ -7,6 +7,12 @@ router.get('/', (req, res, next) => {
     res.render('./docs/home');
 });
 
+router.get('/soccer-sim', (req, res, next) => {
+    if(req.query.page)
+        res.render(`./docs/soccer-sim/${req.query.page}`, {page: parseInt(req.query.page)});
+    else
+        res.render(`./docs/soccer-sim/1`, {page: 0});
+});
 router.get('/soccer-light-python', (req, res, next) => {
     if(req.query.page)
         res.render(`./docs/soccer-light-python/${req.query.page}`, {page: parseInt(req.query.page)});
